ea5e98ae84b92f64ecdb36195f7f70b47d90bc56
Mikhail Ustyantsev <ustyantsevma@gmail.com>
2024-04-23 16:50:26 +0500

    Begin implementing Database manager

---
+++ OnlineStore/Manager/DatabaseManager/DatabaseManager.swift
@@ -0,0 +1,18 @@
+//
+//  DatabaseManager.swift
+//  OnlineStore
+//
+//  Created by Mikhail Ustyantsev on 23.04.2024.
+//
+
+import Foundation
+import FirebaseDatabase
+
+final class DatabaseManager {
+    static let shared = DatabaseManager()
+    
+    private init() {}
+    
+    
+    
+}
