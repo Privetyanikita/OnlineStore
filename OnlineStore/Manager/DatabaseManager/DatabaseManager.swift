04f49924a9ed77dfc016ac763f201af4a5dcdd96
Mikhail Ustyantsev <ustyantsevma@gmail.com>
2024-04-23 22:03:26 +0500

    Continue database manager

--- OnlineStore/Manager/DatabaseManager/DatabaseManager.swift
+++ OnlineStore/Manager/DatabaseManager/DatabaseManager.swift
@@ -8,11 +8,47 @@
 import Foundation
 import FirebaseDatabase
 
+enum DatabaseError: String, Error {
+    case unableToFetch   = "Unable to get your products from database. Please try again"
+}
+
 final class DatabaseManager {
+    
     static let shared = DatabaseManager()
+    private let database = Database.database().reference()
+    
     
     private init() {}
+}
+
+
+extension DatabaseManager {
+    
+    public func insertProduct(with product: Product, completion: @escaping (Bool) -> Void) {
+        let productDetail: [String: Any] = [
+            "product_id": product.id,
+            "product_title": product.title,
+            "product_price": product.price,
+            "product_description": product.description,
+            "product_images": product.images
+        ]
+        database.child("products").setValue(productDetail) { error, _ in
+            guard error == nil else {
+                completion(false)
+                return
+            }
+            completion(true)
+        }
+    }
     
     
-    
+    public func getAllProducts(completion: @escaping (Result<[[String: Any]], DatabaseError>) -> Void) {
+        database.child("products").observeSingleEvent(of: .value) { snapshot, _  in
+            guard let value = snapshot.value as? [[String: Any]] else {
+                completion(.failure(DatabaseError.unableToFetch))
+                return
+            }
+            completion(.success(value))
+        }
+    }
 }
