7657989da8de3193f00d1be2e8337ef3b966e692
Mikhail Ustyantsev <ustyantsevma@gmail.com>
2024-04-25 06:33:35 +0500

    Add func getProductByID

--- OnlineStore/Manager/DatabaseManager/DatabaseManager.swift
+++ OnlineStore/Manager/DatabaseManager/DatabaseManager.swift
@@ -11,7 +11,7 @@ import FirebaseAuth
 
 enum DatabaseError: String, Error {
     case unableToFetchProducts = "Unable to get your products from database. Please try again"
-    case unableToFetchUsers = "Unable to get users from database. Please try again"
+    case failedToGetProduct = "Unable to get your product from database. Please try again"
 }
 
 final class DatabaseManager {
@@ -59,4 +59,28 @@ extension DatabaseManager {
             completion(.success(value))
         }
     }
+    
+    
+    
+    public func getProductByID(with uid: String, completion: @escaping (Result<[String: Any], Error>) -> Void) {
+            let query: DatabaseQuery = database.child("products").queryOrdered(byChild: "product_id").queryEqual(toValue: uid)
+            query.observeSingleEvent(of: .value, with: { (snapshot) in
+                for child in snapshot.children {
+                    let key = (child as AnyObject).key as String
+                    self.database.child("products").child(key).observeSingleEvent(of: .value, with: { snapshot in
+                        guard let value = snapshot.value as? [String: Any] else {
+                            completion(.failure(DatabaseError.failedToGetProduct))
+                            return
+                        }
+                        completion(.success(value))
+                    })
+                }
+            }) { (error) in
+                print(error.localizedDescription)
+            }
+        }
+    
+    
+    
+    
 }
